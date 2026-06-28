ce3acbde5d7e32b0d20b72276de141757cbe5127
himes19 <himes19@gmail.com>
2026-06-27 20:18:38 -0600

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,6 +0,0 @@
-const { handleCors } = require('../lib/auth');
-module.exports = async function handler(req, res) {
-  if (handleCors(req, res)) return;
-  if (req.method !== 'POST') { res.status(405).json({ error: 'Method Not Allowed' }); return; }
-  res.status(200).json({ ok: true });
-};
